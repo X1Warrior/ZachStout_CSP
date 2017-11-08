a56b2e7df159590eb31f3b7865de7eb44f0a79be
X1Warrior <zsto7638@gmail.com>
2017-11-08 12:57:51 -0700

    Changed view names to make more sense as well as linked screens and labels.

---
+++ Controller/CreativityViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CreativityController.swift
+//  ZachStout_CSP
+//
+//  Created by Stout, Zachary on 10/26/17.
+//  Copyright © 2017 Stout, Zachary. All rights reserved.
+//
+
+import UIKit
+
+class CreativityViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
