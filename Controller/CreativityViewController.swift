e02008c49ad73625938966b4661154fc0db72d3c
X1Warrior <zsto7638@gmail.com>
2017-12-07 12:39:23 -0700

    Added code to make the image selctor enlarge the photo when clicked.

--- Controller/CreativityViewController.swift
+++ Controller/CreativityViewController.swift
@@ -14,7 +14,7 @@ class CreativityViewController: UICollectionViewController, UICollectionViewDele
     private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
     private let itemsPerRow : CGFloat = 3
     
-    private lazy var artSeletion : [UIImage?] =
+    private lazy var artSelection : [UIImage?] =
     {
         return [
             UIImage(named: "cute"),
@@ -39,7 +39,7 @@ class CreativityViewController: UICollectionViewController, UICollectionViewDele
                 {
                     indexPaths.append(largePhotoIndexPath)
                 }
-                if let olValue = oldValue
+                if let oldValue = oldValue
                 {
                     indexPaths.append(oldValue)
                 }
@@ -59,17 +59,90 @@ class CreativityViewController: UICollectionViewController, UICollectionViewDele
         }
     }
     
-    
-    override func viewDidLoad() {
+    //MARK: Lifecycle methods
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK:- UICollectionView methods
+   
+    override public func numberOfSections(in collectionView: UICollectionView) -> Int
+    {
+       return 1
     }
     
+    override public func collectionView(_ collectionView: UICollectionView,
+                                            numberOfItemsInSection section: Int) -> Int
+    {
+    return artSelection.count
+    }
     
+    override public func collectionView(_ collectionView: UICollectionView,
+                                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
+    
+    {
+        let artCell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: IndexPath) as! ArtCell
+        
+        artCell.backgroundColor = .purple
+        artCell.imageView.image = artSelection[indexPath.row]
+        artCell.imageName.text = "My Art"
+        
+        return artCell
+    }
+    
+    //MARK:- Delegate methods
+    
+    public func collectionView(_ collectionView: UICollectionView,
+                               layout collectionViewLayout: UICollectionViewLayout,
+                               sizeForItemAt indexPath: IndexPath) -> CGSize
+    {
+        if indexPath == largePhotoIndexPath
+        {
+            let art = artSelection[indexPath.row]
+            let size = collectionView.bounds.size
+            let widthScale = (size.width / art!.size.width) * CGFloat(0.80)
+            let largeSize = CGSize(width: art!.size.width * widthScale, height: art!.size.height * widthScale)
+            
+            return largeSize
+        }
+        
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+        
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView,
+                               layout collectionViewLayout: UICollectionViewLayout,
+                               insetForSection: Int) -> UIEdgeInsets
+    {
+        return sectionInsets
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView,
+                               layout colleonViewLayout: UICollectionViewLayout,
+                               minimumLineSpacingForSectionAt section: Int) -> CGFloat
+    {
+    return sectionInsets.left
+    }
+    
+    override public func collectionView(_ collectionView: UICollectionView,
+                                        shouldSelectItemAt indexPath: IndexPath) -> Bool
+    {
+        if largePhotoIndexPath == indexPath
+        {
+            largePhotoIndexPath = nil
+        }
+        else
+        {
+            largePhotoIndexPath = indexPath
+        }
+        
+        return false
+    }
 }
+
 
