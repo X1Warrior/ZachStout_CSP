af9238d80eb4049fa7244fc40dee4d0237290653
X1Warrior <zsto7638@gmail.com>
2018-01-09 11:40:34 -0700

    Created bucket list classes for project.

--- Controller/DataView/DataViewController.swift
+++ Controller/DataView/DataViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class DataViewController: UIViewController {
+class DataViewController: UITableViewController
+{
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Model/BucketItem.swift
@@ -0,0 +1,24 @@
+//
+//  BucketItem.swift
+//  ZachStout_CSP
+//
+//  Created by Stout, Zachary on 1/9/18.
+//  Copyright © 2018 Stout, Zachary. All rights reserved.
+//
+
+import UIKit
+
+class BucketItem: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
